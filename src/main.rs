e623f5adcb8fda7e83dfcbce7e2effaa5e487c53
Kat-Luk <katarinalukacova6@gmail.com>
2026-06-26 18:56:56 +0200

    Refactor money handling in create_threads function to use Arc and Mutex for thread safety

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ use std::thread;
 use std::time::Duration;
 use std::io;
 use std::io::Write;
+use std::sync::{Arc, Mutex};
 
 fn main() {
-    let mut money = 0;
+    let mut money: i64 = 0;
     println!("Do you have a million dollars? | y = yes, n = no");
     let mut choice = String::new();
     io::stdin().read_line(&mut choice).expect("Error reading input.");
@@ -23,28 +24,34 @@ fn main() {
     create_threads(money);
 }
 
-fn create_threads(mut money: i64) {
+fn create_threads(money: i64) {
+    let money = Arc::new(Mutex::new(money));
+    let starting_amount = *money.lock().unwrap();
     let (tx, rx) = mpsc::channel();
 
+    let money1 = Arc::clone(&money);
     let tx1 = tx.clone();
     thread::spawn(move || {
-        if money > 600000 {
-            loop{
-                thread::sleep(Duration::from_secs(3));
-                println!("ALERT!!! Someone stole $35,000 from you!");
-                tx1.send(money-35000).unwrap();
-            }
+        loop{
+            thread::sleep(Duration::from_secs(3));
+            let mut m = money1.lock().unwrap();
+            if *m <= 600000 { break; }
+            *m -= 35000;
+            println!("ALERT!!! Someone stole $35,000 from you!");
+            tx1.send(*m).unwrap();
         }
     });
 
+    let money2 = Arc::clone(&money);
     let tx2 = tx.clone();
     thread::spawn(move || {
-        if money > 10000-1{
-            loop{
-                thread::sleep(Duration::from_secs(5));
-                println!("ALERT!!! Someone stole $10,000 from you!");
-                tx2.send(money-10000).unwrap();
-            }
+        loop{
+            thread::sleep(Duration::from_secs(5));
+            let mut m = money2.lock().unwrap();
+            if *m <= 0 { break; }
+            *m -= 10000;
+            println!("ALERT!!! Someone stole $10,000 from you!");
+            tx2.send(*m).unwrap();
         }
     });
 
